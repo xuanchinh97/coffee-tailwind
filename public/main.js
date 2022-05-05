5751762af29764db4a933555ee36e4a38b4814e4
chinhdz97 <kqtchcheng97@gmail.com>
2022-05-05 20:47:52 +0700

    responsive header

---
+++ public/main.js
@@ -0,0 +1,19 @@
+
+const topMenu = document.querySelector('#top-menu')
+const toggleTopMenu = document.querySelector('#toggle-top-menu')
+
+document.addEventListener('click', (e) => {
+    // click to toogle top menu icon 
+    if (toggleTopMenu.contains(e.target)) {
+        topMenu.classList.toggle('hidden')
+        topMenu.classList.toggle('ct-top-menu-expanded')
+    } else {
+        // click outside from toggle top menu icon 
+        if (topMenu.classList.contains('ct-top-menu-expanded')) {
+            topMenu.classList.remove('ct-top-menu-expanded')
+            topMenu.classList.add('hidden')
+        }
+
+    }
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     extend: {
       colors: {
         'light-coffee': '#C89F94'
+      },
+      fontFamily: {
+        Karla: ['Karla', 'sans-serif']
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        slideDown: 'slideDown .2s ease-in-out',
       }
     },
   },
