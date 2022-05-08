ab5c97d173799413a04578af83a25b48da674d87
chinhdz97 <kqtchcheng97@gmail.com>
2022-05-08 21:33:54 +0700

    add scroll to top

--- public/main.js
+++ public/main.js
@@ -1,4 +1,10 @@
 
+
+// init animation 
+AOS.init();
+
+
+// menu
 const topMenu = document.querySelector('#top-menu')
 const toggleTopMenu = document.querySelector('#toggle-top-menu')
 
@@ -17,5 +23,24 @@ document.addEventListener('click', (e) => {
     }
 })
 
-AOS.init();
+//Get the button:
+const btnScrollTop = document.querySelector("#scroll-to-top")
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnScrollTop.style.display = "block";
+    } else {
+        btnScrollTop.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    window.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
+}
+
+btnScrollTop.addEventListener("click" , topFunction)
